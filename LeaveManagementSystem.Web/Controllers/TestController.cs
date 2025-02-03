4b26b7b9b9d0320bf4fb68af8afd99058b95a888
am ir <amirmohammadshi@hotmail.com>
2025-02-03 22:37:24 +0330

    routine

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+    {
+    public class TestController : Controller
+        {
+        public IActionResult Index ()
+            {
+            var data = new TestViewModel
+                {
+                Name = "Amir"    ,
+                DateOfBirth = new DateTime(1983,01,09)
+                };
+            return View(data);
+            }
+        }
+    }
