c1ce7b6da2008be8c748008acbddbddd0607cee8
am ir <amirmohammadshi@hotmail.com>
2025-02-05 00:32:12 +0330

    update-db via adding identity tables

--- LeaveManagementSystem.Web/Program.cs
+++ LeaveManagementSystem.Web/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("MyLocalConnection") ?? throw new InvalidOperationException("Connection string 'MyLocalConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
