4d9cf64e7c6833a18ad4dd294459efaa9c1ec425
am ir <amirmohammadshi@hotmail.com>
2025-02-07 00:21:15 +0330

    adding LeaveType Entity and its Controllers into MVC

--- LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System.Drawing;
+using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagementSystem.Web.Data
@@ -9,5 +11,20 @@ namespace LeaveManagementSystem.Web.Data
             : base(options)
         {
         }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    builder.Entity<LeaveType>(lt =>
+        //    {
+        //        lt.HasKey(e => e.Id);
+        //        lt.Property(e => e.Id)
+        //            .ValueGeneratedOnAdd() // Client-side
+        //            .HasDefaultValueSql("NEWID()"); // Database-side
+        //    }
+        //    );
+
+        //}
     }
 }
---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementSystem.Web.Data;
+
+public class LeaveType
+{
+    [Key]
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int NumberOfDays { get; set; }
+}
